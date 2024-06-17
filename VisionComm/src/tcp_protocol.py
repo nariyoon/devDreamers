941647079c048e4dafaf6f04394d689edfffb98d
bhShin1020 <bhshin84@gmail.com>
2024-06-17 13:09:27 -0400

    Add send/recv command function

--- VisionComm/src/tcp_protocol.py
+++ VisionComm/src/tcp_protocol.py
@@ -23,14 +23,17 @@ class TMesssageHeader:
         self.Len = len_
         self.Type = type_
 
+clientSock = 0
+
 def tcp_ip_thread():
     """
     This thread handles TCP/IP communication with the Raspberry Pi.
     """
-    print("start ip thread!!!!!!!!!!!!!!!")
+    print("start receiving image thread")
     host = '192.168.0.224'  # Localhost for testing, change to Raspberry Pi IP
     port = 5000             # Port to listen on
 
+    global clientSock
     serverAddress = (host, port)
     clientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -44,49 +47,49 @@ def tcp_ip_thread():
     while True:
         try:
             # Receive the message header
-            header_data = clientSock.recv(8)
-            if len(header_data) != struct.calcsize('II'):
+            headerData = clientSock.recv(8)
+            if len(headerData) != struct.calcsize('II'):
                 print("Connection lost.")
                 break
 
             # Unpack the message header
-            len_, type_ = struct.unpack('II', header_data)
+            len_, type_ = struct.unpack('II', headerData)
             len_ = socket.ntohl(len_)
             type_ = socket.ntohl(type_)
-            print("header len_ ", len_, "header type_ ", type_)
-
-            # Buffer to store the received message
-            buffer = bytearray(len_)
-            print("buffer lenn ", len(buffer))
-
-            # Receive data into the buffer
-            bytes_received = 0
-            while bytes_received < len_:
-                chunk = clientSock.recv(len_ - bytes_received)
-                if not chunk:
-                    # Handle error or connection closed
-                    break
-                buffer[bytes_received:] = chunk
-                bytes_received += len(chunk)
-
-            # Check if all expected bytes have been received
-            if bytes_received != len_:
-                # Handle incomplete message reception
-                pass
-
-            # Receive the message body
-            #if len(body_data) != len_:
-            #    print("Connection lost.")
-            #    break
+            #print("header len_ ", len_, "header type_ ", type_)
 
             if type_ == MT_IMAGE:
+                # Buffer to store the received message
+                buffer = bytearray(len_)
+                #print("buffer lenn ", len(buffer))
+
+                # Receive data into the buffer
+                bytesReceived = 0
+                while bytesReceived < len_:
+                    chunk = clientSock.recv(len_ - bytesReceived)
+                    if not chunk:
+                        # Handle error or connection closed
+                        break
+                    buffer[bytesReceived:] = chunk
+                    bytesReceived += len(chunk)
+
+                # Check if all expected bytes have been received
+                if bytesReceived != len_:
+                    pass
+
+                # Receive the message body
+                #if len(body_data) != len_:
+                #    print("Connection lost.")
+                #    break
+
                 # Process the received message based on its typeSsizeof(TMesssageHeader)), cv::IMREAD_COLOR, &ImageIn);
                 imageMat = cv2.imdecode(np.frombuffer(buffer, dtype=np.uint8), cv2.IMREAD_COLOR)
-                #ip.recvCameraImage(imageMat)
                 cv2.imshow('camera', imageMat)
                 cv2.waitKey(1)
             else:
                 print("bypass to UI")
+                msg = clientSock.recv(512)
+                sendMsgToUI(msg)
                 #sendToUi
 
         except socket.error as e:
@@ -97,5 +100,20 @@ def tcp_ip_thread():
             break
 
     cv2.destroyAllWindows()
+    clientSock.close()
     print("Network Thread Exit")
-    return None
+
+def sendMsgToCannon(msg):
+    print("recieve the msg. from UI for sending msg. to cannon(len: ", len(msg), ")")
+    global clientSock
+    type = msg[5:8]
+
+    if type == MT_TARGET_SEQUENCE:
+        # send to image process
+        print("type is MT_TARGET_SEQUENCE")
+    else:
+        clientSock.sendall(msg)
+
+def sendMsgToUI(msg):
+    print("send command to UI(len: ", len(msg), ")")
+    # send callback to UI
