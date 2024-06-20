5bf0b41f5a7514356efe3f658b1a7e5cd25aec3e
Seulhui Lee <hoisulhi@naver.com>
2024-06-20 17:11:55 -0400

    Add command and struct for target point

--- Common/Message.h
+++ Common/Message.h
@@ -12,6 +12,7 @@
 #define MT_STATE                 6
 #define MT_STATE_CHANGE_REQ      7
 #define MT_CALIB_COMMANDS        8
+#define MT_TARGET_DIFF           9
 
 #define PAN_LEFT_START  0x01
 #define PAN_RIGHT_START 0x02
@@ -103,6 +104,14 @@ typedef struct
     TMesssageHeader Hdr;
     unsigned char  Commands;
 } TMesssageCalibCommands;
+
+typedef struct
+{
+    TMesssageHeader Hdr;
+    int target;
+    float Pan;
+    float Tilt;
+} TMesssageNextTargetDiff;
 
 #endif
 //------------------------------------------------------------------------------------------------
